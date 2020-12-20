e830a36f1f3336980be797e38a3bc8f498512bfa
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-20 17:40:32 -0500

    bootstrapping class that populates the DB with a few User entities

--- src/main/java/com/simple/user/application/simpleuserapplication/SimpleuserapplicationApplication.java
+++ src/main/java/com/simple/user/application/simpleuserapplication/SimpleuserapplicationApplication.java
@@ -1,13 +1,30 @@
 package com.simple.user.application.simpleuserapplication;
 
+import com.simple.user.application.simpleuserapplication.Repository.UserRepository;
+import com.simple.user.application.simpleuserapplication.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SimpleuserapplicationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleuserapplicationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository){
+		return args -> {
+			Stream.of("Jonh", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
